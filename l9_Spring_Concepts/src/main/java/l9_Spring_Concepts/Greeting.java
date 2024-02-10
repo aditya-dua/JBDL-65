108139789c72509ad16c22a860364dddd03cdf69
aditya-dua <adityaduatechm@gmail.com>
2024-02-10 22:28:59 +0530

    Beans

--- l9_Spring_Concepts/src/main/java/l9_Spring_Concepts/Greeting.java
+++ l9_Spring_Concepts/src/main/java/l9_Spring_Concepts/Greeting.java
@@ -12,8 +12,30 @@ public class Greeting {
 		this.message = message;
 	}
 	
+	public Greeting() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Greeting(String message) {
+		super();
+		this.message = message;
+	}
+	
+	public Greeting(int message) {
+		super();
+		System.out.println("Int based Constructor called");
+	}
+
 	public void display() {
 		System.out.println("Message :: "+message);
 	}
 
+	public void init() {
+		System.out.println("Init called");
+	}
+	
+	public void killGreeting() {
+		System.out.println("killGreeting called");
+	}
 }
--- l9_Spring_Concepts/src/main/java/l9_Spring_Concepts/SpringMain.java
+++ l9_Spring_Concepts/src/main/java/l9_Spring_Concepts/SpringMain.java
@@ -1,6 +1,7 @@
 package l9_Spring_Concepts;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringMain {
@@ -11,14 +12,24 @@ public class SpringMain {
 		System.out.println("Context:"+context);
 		
 		Greeting greet = (Greeting) context.getBean("greeting");
+		Greeting greet3 = (Greeting) context.getBean("greeting");
+		
 		
 		//greet.setMessage("Hello World");
 		
 		greet.display();
 		
 		Greeting greet2 = (Greeting) context.getBean("greeting2");
+		Greeting greet22 = (Greeting) context.getBean("greeting2");
 		
 		greet2.display();
+		
+		System.out.println(greet);
+		System.out.println(greet2);
+		System.out.println(greet3);
+		System.out.println(greet22);
+		
+		((AbstractApplicationContext) context).registerShutdownHook();
 	}
 
 }
