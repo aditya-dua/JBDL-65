0c84066353341248c6dc092049cd8fe4b924a38b
aditya-dua <adityaduatechm@gmail.com>
2024-03-30 21:57:14 +0530

    Logging File
    Logging File

--- L21_Embedded_Server_Examples/src/main/java/com/example/demo/L21EmbeddedServerExamplesApplication.java
+++ L21_Embedded_Server_Examples/src/main/java/com/example/demo/L21EmbeddedServerExamplesApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class L21EmbeddedServerExamplesApplication {
 
 	public static void main(String[] args) {
+		
+		Logger logger = LoggerFactory.getLogger("abc");
+		
+		logger.info("This is an Info Log");
+		logger.warn("This is an Warning Log");
+		logger.debug("This is an Debug Log");
+		logger.trace("This is an Trace Log");
 		SpringApplication.run(L21EmbeddedServerExamplesApplication.class, args);
 	}
+	/**
+	 * ERROR : severe and minmum
+	 * INFO: know and debug
+	 * DEBUG: INFO + Extra debugging
+	 * TRACE: slighest change...hearbeat 
+	 * @return
+	 */
 	
 	@GetMapping("/test")
 	public String test() {
