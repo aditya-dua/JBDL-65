080a2f9f7de76bb5f77322a7ea204c46f5a93e92
aditya-dua <adityaduatechm@gmail.com>
2024-03-31 22:41:15 +0530

    Junit
    Junit

---
+++ L22_JUnit_Testing/src/AddFunctionalityTesting.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AddFunctionalityTesting {
+
+	static Calculator c;
+	
+	@BeforeClass
+	public static void setup() {
+		c = new Calculator();
+		System.out.println("Before Class Called");
+	}
+	
+	@Before
+	public void setupBefore() {
+		
+		System.out.println("Before Called");
+	}
+	
+	@Test
+	public void test() {
+		int sum  = c.add(10, 20);
+		assertEquals(35, sum);
+	}
+	
+	@Test
+	public void diffTest() {
+		int diff  = c.diff(10, 20);
+		assertEquals(-10, diff);
+		
+	}
+	
+	@After
+	public void afterTest() {
+		//c = null;
+		System.out.println("After Called");
+	}
+	
+
+}
---
+++ L22_JUnit_Testing/src/Calculator.java
@@ -0,0 +1,18 @@
+
+public class Calculator {
+	
+	public int add(int a, int b) {
+		return a+b;
+		
+	}
+	
+	public int diff(int a, int b) {
+		return a-b;
+		
+	}
+	
+	public int calculateInterest(int p,int r, int t) {
+		return p*r*t/100;
+	}
+
+}
