d88273e0df04123b8a3ecd1561da8396c644a651
aditya-dua <adityaduatechm@gmail.com>
2024-04-14 20:59:43 +0530

    Upload Multi Part File in SB
    Upload Multi Part File in SB

---
+++ L26_Parsing_CSV_SB/src/main/java/com/example/demo/FileController.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+
+public class FileController {
+
+	
+	@PostMapping("/upload")
+	public String uploadFile(@RequestParam("file") MultipartFile file) {
+		
+		
+		
+		
+		try {
+			byte[] fileBytes = file.getBytes();
+			Path path = Paths.get("/Users/adityadua/Documents/JBDL-65/"+file.getOriginalFilename());
+			
+			Files.write(path, fileBytes);
+			
+			return "File uploaded Successfully :: "+file.getOriginalFilename()+" "+path;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+			return "File uploaded Error!";
+		}
+		
+		
+		
+	}
+}
---
+++ L26_Parsing_CSV_SB/src/main/java/com/example/demo/L26ParsingCsvSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L26ParsingCsvSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L26ParsingCsvSbApplication.class, args);
+	}
+
+}
